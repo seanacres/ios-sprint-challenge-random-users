059cf4e2e181ad238d28ae0997d1dee7205278a8
seanacres <6416690+seanacres@users.noreply.github.com>
2019-08-09 16:45:30 -0400

    Call isfinished if it can’t get a URL

--- Random Users/Operations/PhotoFetchOperation.swift
+++ Random Users/Operations/PhotoFetchOperation.swift
@@ -12,7 +12,7 @@ class PhotoFetchOperation: ConcurrentOperation {
     var imageURL: String
     var imageData: Data?
     
-    private var dataTask: URLSessionDataTask = URLSessionDataTask()
+    private var dataTask: URLSessionDataTask?
     
     // MARK: Functions
     init(imageURL: String) {
@@ -20,29 +20,36 @@ class PhotoFetchOperation: ConcurrentOperation {
     }
     
     override func start() {
+        if isCancelled { return }
         state = .isExecuting
-        if let url = URL(string: imageURL) {
-            let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
-                defer {
-                    self.state = .isFinished
-                }
-                
-                if let error = error {
-                    NSLog("Error getting image data from url: \(error)")
-                    return
-                }
-                
-                guard let data = data else { return }
-                self.imageData = data
-                
+        
+        guard let url = URL(string: imageURL) else {
+            state = .isFinished
+            return
+            
+        }
+
+        let task = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            defer {
+                self.state = .isFinished
             }
             
-            task.resume()
-            self.dataTask = task
+            if let error = error {
+                NSLog("Error getting image data from url: \(error)")
+                return
+            }
+            
+            guard let data = data else { return }
+            self.imageData = data
+            
         }
+        
+        task.resume()
+        self.dataTask = task
     }
     
     override func cancel() {
-        dataTask.cancel()
+        super.cancel()
+        dataTask?.cancel()
     }
 }
